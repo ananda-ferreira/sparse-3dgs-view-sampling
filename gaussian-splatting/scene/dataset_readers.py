9507cffdee0aef072464bce9d20be7335cc25956
Ananda Ferreira <ananda.ferreiragomes@gmail.com>
2026-03-20 11:25:19 +0100

    retry

--- gaussian-splatting/scene/dataset_readers.py
+++ gaussian-splatting/scene/dataset_readers.py
@@ -22,7 +22,7 @@ from pathlib import Path
 from plyfile import PlyData, PlyElement
 from utils.sh_utils import SH2RGB
 from scene.gaussian_model import BasicPointCloud
-from dataset_readers_helper import readSparseColmapSceneInfo
+from scene.dataset_readers_test import readSparseColmapSceneInfo
 
 class CameraInfo(NamedTuple):
     uid: int
