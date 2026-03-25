7193502486a224bb7a5bef66a6ec5caec3aa369a
Ananda Ferreira <ananda.ferreiragomes@gmail.com>
2026-03-25 10:16:32 +0100

    debug

--- gaussian-splatting/samplers/sampler.py
+++ gaussian-splatting/samplers/sampler.py
@@ -30,7 +30,9 @@ class Sampler:
         """ Returns: a list of tuples, where each tuple holds 2 camera centers (3D np arrays) corresponding to the pairs of images in views. """
         if views == None:
             views = self.sparse_views
-        return [c["C"] for c in self.cam_infos_ext if c["img_name"] in views]
+        sparse_cs = [c["C"] for c in self.cam_infos_ext if c["img_name"] in views]
+        print(f"sparse_cs: {sparse_cs}")
+        return sparse_cs
     
     def save_sparse_views(self, model_path):
         if not os.path.exists(model_path):
