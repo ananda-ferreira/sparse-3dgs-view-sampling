7ca326f3430db5c5b1a999f498619871905757bf
Ananda Ferreira <ananda.ferreiragomes@gmail.com>
2026-03-31 16:11:50 +0200

    print for euclid pairwise dist analysis

--- gaussian-splatting/samplers/utils_sampler.py
+++ gaussian-splatting/samplers/utils_sampler.py
@@ -1,21 +1,21 @@
 import numpy as np
 
-# # following github: uni excercise
-# def calc_pairwise_distances(points: list):
-#     pts = np.array(points)
-#     # broadcasting: None inserts a new axis, to get a N,N,3 matrix (for pts.shape = N,3)
-#     diff = pts[:, None, :] - pts[None, :, :]
-#     # L2 norm for euclidean distance, axis -1 for last axis = xyz vectors
-#     out = np.linalg.norm(diff, axis=-1) 
-#     print(f"euclid dist matrix: {out}")
-#     return out
-
+# following github: uni excercise
 def calc_pairwise_distances(points: list):
     pts = np.array(points)
-    # None inserts another axis, to get a N,N,3 matrix, for pts.shape = N,3
-    out = np.linalg.norm(pts[:, None, :] - pts[None, :, :], axis=1)
-    print(f"euclid dist matrix ax 1: {out}") 
+    # broadcasting: None inserts a new axis, to get a N,N,3 matrix (for pts.shape = N,3)
+    diff = pts[:, None, :] - pts[None, :, :]
+    # L2 norm for euclidean distance, axis -1 for last axis = xyz vectors
+    out = np.linalg.norm(diff, axis=-1) 
+    print(f"euclid dist matrix ax -1: {out}")
     return out
+
+# def calc_pairwise_distances(points: list):
+#     pts = np.array(points)
+#     # None inserts another axis, to get a N,N,3 matrix, for pts.shape = N,3
+#     out = np.linalg.norm(pts[:, None, :] - pts[None, :, :], axis=1)
+#     print(f"euclid dist matrix ax 1: {out}") 
+#     return out
 
 # def calc_pairwise_distances(points: list):
 #     # this can be improved by avoiding the squareroot computation of euclidean distance
